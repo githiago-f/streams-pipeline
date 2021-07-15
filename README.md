77519153ef4737421ce38134a3618b5e1ec099c2
githiago-f <tfarias@protonmail.com>
2021-07-15 15:22:10 -0300

    create streamming logic 💻🏞🌅

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # streams-pipeline
 Node JS streams pipeline testing
+
+
+## Running this app
+
+First things first, add a video to "express/videos/video.mp4".
+
+Then you're good to go:
+
+```bash
+$ yarn start
+```
--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-const fs = require("fs");
+const { createReadStream, promises } = require("fs");
+const express = require('express');
+const { resolve } = require("path");
 
-const readable = fs.createReadStream('/examples/text.txt');
+//## DEFAULT VALUES
+const path = resolve(__dirname, '../express/videos/video.mp4');
+const CHUNK_SIZE = 10 ** 5;
+//## END DEFAULT VALUES
+
+const app = express();
+
+app.use(express.static(resolve(__dirname, '../public')));
+
+app.get('/video', async (req, res) => {
+  const { range } = req.headers;
+  const { size } = await promises.stat(path);
+
+  if (!range) {
+    return res.status(400).send('Requires range header');
+  }
+
+  const
+    start = Number((range || '').replace(/bytes=/, '').split('-')[0]),
+    end = Math.min(start + CHUNK_SIZE, size - 1);
+
+  const headers = {
+    'Accept-Ranges': 'bytes',
+    'Content-Range': `bytes ${start}-${end}/${size}`,
+    'Content-Length': (end - start) + 1,
+    'Content-Type': 'video/mp4'
+  };
+
+  res.writeHead(206, headers);
+
+  const stream = createReadStream(path, { start, end });
+  stream.on('open', () => stream.pipe(res))
+    .on('close', err => res.end(err));
+});
+
+app.listen(3000, () => {
+  console.log('LISTENING');
+});
